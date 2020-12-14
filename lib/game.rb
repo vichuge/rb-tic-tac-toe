508736952e07b0ad982cc4e47d33cf620cd6bdc7
Victor Hugo Pacheco Flores <victor.hugo.pacheco.flores@gmail.com>
2020-12-14 14:23:24 -0600

    Revert "Development Branch Update"

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,14 @@
+# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/AbcSize
+
 class Game
-  attr_accessor :game, :name, :players, :player_turn
-  attr_reader :winner
+  attr_accessor :game
+  attr_reader :winner, :player
 
   def initialize
     @winner = nil
     @game = Array.new(10)
     @game[0] = 'empty'
-    @players = []
-    @player_turn = 0
+    @player = 'X'
   end
 
   def see_game
@@ -20,29 +21,42 @@ class Game
     elsif !@game[move].nil?
       "error! That position is already taken\n"
     else
-      @game[move] = player.symbol
-      player.moves.push(move)
-      win(player.moves, player.name)
+      @game[move] = player
+      win
       change_turn
-      "\n#{player.name}, you selected position #{move}. Now your move is displayed on the board.\n"
+      "\nYou have selected position #{move}. Now your move is displayed on the board.\n"
     end
   end
 
   def change_turn
-    self.player_turn = @player_turn.zero? ? 1 : 0
+    @player = @player == 'X' ? 'O' : 'X'
   end
 
   def print_winner
-    @winner == 'draw' ? 'It\'s a draw!' : "Congrats, #{@winner}! You won this match!"
+    @winner == 'draw' ? 'It\'s a draw!' : "Congrats, the winner is #{@winner}"
   end
 
-  def win(moves, player)
-    if moves.length > 2
-      permutations = moves.permutation(3).to_a
-      permutations = permutations.select { |el| el[1] > el[0] && el[1] < el[2] }
-      return @winner = player unless (permutations & [[1, 2, 3], [7, 8, 9], [1, 4, 7], [3, 6, 9],
-                                                      [4, 5, 6], [2, 5, 8], [1, 5, 9], [3, 5, 7]]).empty?
+  def win
+    if @game[1] == @game[2] && @game[2] == @game[3]
+      @winner = @game[1]
+    elsif @game[4] == @game[5] && @game[5] == @game[6]
+      @winner = @game[4]
+    elsif @game[7] == @game[8] && @game[8] == @game[9]
+      @winner = @game[7]
+    elsif @game[1] == @game[4] && @game[4] == @game[7]
+      @winner = @game[1]
+    elsif @game[2] == @game[5] && @game[5] == @game[8]
+      @winner = @game[2]
+    elsif @game[3] == @game[6] && @game[6] == @game[9]
+      @winner = @game[3]
+    elsif @game[1] == @game[5] && @game[5] == @game[9]
+      @winner = @game[1]
+    elsif @game[3] == @game[5] && @game[5] == @game[7]
+      @winner = @game[3]
+    elsif @game.all?
+      @winner = 'draw'
     end
-    @winner = 'draw' if @game.all?
   end
 end
+
+# rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/AbcSize
