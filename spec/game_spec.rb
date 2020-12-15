0314e0d38994c2156622fcf0654eaf44f4d7847e
Miguel Ricaño <mricanho@gmail.com>
2020-12-15 12:11:37 -0600

    Add more test cases

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,12 +23,80 @@ describe Game do
 
   describe '#print_winner' do
     it 'Show the winner or draw according the game' do
-      expect(game.print_winner).to eql('Congrats, the winner is ')
+      game.turn(1, 'X')
+      game.turn(2, 'O')
+      game.turn(4, 'X')
+      game.turn(3, 'O')
+      game.turn(7, 'X')
+      game.win
+      expect(game.print_winner).to eql('Congrats, the winner is X')
+    end
+
+    it 'Show the winner or draw according the game' do
+      game.turn(5, 'X')
+      game.turn(2, 'O')
+      game.turn(4, 'X')
+      game.turn(3, 'O')
+      game.turn(7, 'X')
+      game.turn(1, 'O')
+      game.win
+      expect(game.print_winner).to eql('Congrats, the winner is O')
+    end
+
+    it 'Show the draw according to the game' do
+      game.turn(5, 'X')
+      game.turn(1, 'O')
+      game.turn(2, 'X')
+      game.turn(3, 'O')
+      game.turn(9, 'X')
+      game.turn(6, 'O')
+      game.turn(7, 'X')
+      game.turn(8, 'O')
+      game.turn(4, 'X')
+      game.win
+      expect(game.print_winner).to eql("It's a draw!")
     end
   end
 
   describe '#win' do
-    it 'Assign the winner after a moverment' do
+    it "return the winner's move, X in this case" do
+      game.turn(1, 'X')
+      game.turn(2, 'O')
+      game.turn(4, 'X')
+      game.turn(3, 'O')
+      game.turn(7, 'X')
+      game.win
+      expect(game.win).to eql('X')
+    end
+
+    it "return the winner's move, O in this case" do
+      game.turn(5, 'X')
+      game.turn(2, 'O')
+      game.turn(4, 'X')
+      game.turn(3, 'O')
+      game.turn(7, 'X')
+      game.turn(1, 'O')
+      game.win
+      expect(game.win).to eql('O')
+    end
+
+    it "return the winner's move, draw in this case" do
+      game.turn(5, 'X')
+      game.turn(1, 'O')
+      game.turn(2, 'X')
+      game.turn(3, 'O')
+      game.turn(9, 'X')
+      game.turn(6, 'O')
+      game.turn(7, 'X')
+      game.turn(8, 'O')
+      game.turn(4, 'X')
+      game.win
+      expect(game.win).to eql('draw')
+    end
+
+    it "return nil if the game don't continue" do
+      game.turn(5, 'X')
+      game.win
       expect(game.win).to eql(nil)
     end
   end
